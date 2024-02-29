c630378234c69d84c73bacf882912ef308146ae4
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-02-29 12:00:09 -0300

    feat: add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,24 @@
+import Author from "@/components/Author";
+import Drawers from "@/components/Drawers";
+import Text from "@/components/Text";
+import Title from "@/components/Title";
+
 export default function Home() {
-  return <h1>hello</h1>;
+  return (
+    <main className="bg-white w-80 overflow-hidden rounded-xl shadow-xl">
+      <Drawers />
+      <section className="flex flex-col gap-4 p-8">
+        <Title>
+          Shift the overall look and feel by adding these wonderful touches to
+          furniture in your home
+        </Title>
+        <Text>
+          Ever been in a room and felt like something was missing? Perhaps it
+          felt slightly bare and uninviting. I’ve got some simple tips to help
+          you make any room feel complete.
+        </Text>
+        <Author />
+      </section>
+    </main>
+  );
 }
