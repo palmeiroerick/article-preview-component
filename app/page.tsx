807045100e134b0630d26692130d2b0c3d40fcd6
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-02-29 12:19:18 -0300

    style: add desktop styles to home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,9 @@ import Title from "@/components/Title";
 
 export default function Home() {
   return (
-    <main className="bg-white w-80 overflow-hidden rounded-xl shadow-xl">
+    <main className="bg-white w-80 overflow-hidden rounded-xl shadow-xl lg:flex lg:h-64 lg:w-[680px]">
       <Drawers />
-      <section className="flex flex-col gap-4 p-8">
+      <section className="flex flex-col gap-4 p-8 justify-between lg:gap-0 lg:p-7">
         <Title>
           Shift the overall look and feel by adding these wonderful touches to
           furniture in your home
