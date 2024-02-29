1d23e8ba0edfcdbcfbf21c57fac8697b9eb0b555
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-02-29 11:33:18 -0300

    chore: add project config

---
+++ tailwind.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./components/**/*.tsx", "./app/**/*.tsx"],
+  theme: {
+    screens: {
+      sm: "375px",
+      lg: "1440px",
+    },
+    colors: {
+      white: "#ffffff",
+      veryDarkGrayishBlue: "hsl(217, 19%, 35%)",
+      desaturatedDarkBlue: "hsl(214, 17%, 51%)",
+      grayishBlue: "hsl(212, 23%, 69%)",
+      lightGrayishBlue: "hsl(210, 46%, 95%)",
+    },
+    fontWeight: {
+      medium: "500",
+      bold: "700",
+    },
+    backgroundImage: {
+      mobile: "url('/assets/bg-mobile.svg')",
+      desktop: "url('/assets/bg-desktop.svg')",
+    },
+  },
+};
+
+export default config;
